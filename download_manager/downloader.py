3e7e331db68623d35f4913512ac0e227dc8f10d1
Jalen Sun <jalensungt@gmail.com>
2025-09-29 18:14:46 -0700

    changed to fit new path with format code-title

--- download_manager/downloader.py
+++ download_manager/downloader.py
@@ -32,6 +32,7 @@ class downloadManager():
         parent_dir = os.path.dirname(script_dir)
         name = get_name(sauce)
         name_safe = sanitize_filename(name)[:75]
+        name_safe = sauce + "-" + name_safe
         folder_path = os.path.join(parent_dir, "Downloads", "Saved_Doujins", name_safe)
         os.makedirs(folder_path, exist_ok=True)
         pdf_file = os.path.join(folder_path, f"{name_safe}.pdf")
@@ -45,7 +46,7 @@ class downloadManager():
             args_list = []
             for idx, img_url in enumerate(results, start=1):
                 file_name = f"Page_{idx:03}{file_ext}"
-                path_file = construct_npath([name, file_name])
+                path_file = construct_npath([name, file_name],sauce)
                 args_list.append((img_url, idx, path_file, headers))
 
             with concurrent.futures.ThreadPoolExecutor(max_workers = 5) as executor:
@@ -56,6 +57,7 @@ class downloadManager():
     def delete(self,doujin):
         doujin_name = get_name(doujin)
         doujin_name =sanitize_filename(doujin_name)[:75]
+        doujin_name = doujin + "-" + doujin_name
         script_dir = os.path.dirname(os.path.abspath(__file__))
         parent_dir = os.path.dirname(script_dir)
         doujin_folder = os.path.join(parent_dir, "Downloads", "Saved_Doujins",doujin_name)
