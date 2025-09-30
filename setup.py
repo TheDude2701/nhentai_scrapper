c6dcd000c039070d17b338e6b4036fe90cd5bd8a
Jalen Sun <jalensungt@gmail.com>
2025-09-30 12:34:23 -0700

    Added new requirements

--- setup.py
+++ setup.py
@@ -1,18 +1,21 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="nHentai",
+    name="nhentai",
     version="1.0",
     py_modules=["main"],
     entry_points={
         "console_scripts": [
-            "nHentai = main:main",
+            "nhentai = nhentai_scrapper.main:main",
         ],
     },
     install_requires=[
         "beautifulsoup4==4.13.3",
         "Pillow==9.3.0",
         "playwright==1.55.0",
-        "requests==2.32.3"
+        "requests==2.32.3",
+        "lxml",
+        "concurrent-futures; python_version<'3.2'"
+
     ],
 )
