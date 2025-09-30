f36622a48c2548ff05ba9c90ca3dc6f17aab9a7f
Jalen Sun <jalensungt@gmail.com>
2025-09-30 13:39:46 -0700

    print pdf path when opening

--- nhentai_scrapper/download_manager/pdfconvert.py
+++ nhentai_scrapper/download_manager/pdfconvert.py
@@ -27,4 +27,5 @@ def images_to_pdf(folder_path):
 def open_pdf(folder_path):
     folder_name = os.path.basename(os.path.normpath(folder_path))
     folder_pdf = os.path.join(folder_path, f"{folder_name}.pdf")
+    print(folder_name)
     os.startfile(folder_pdf)
