165d7877e031fbd27106766977344eb63d026bf1
Jalen Sun <jalensungt@gmail.com>
2025-09-29 18:15:44 -0700

    Changed the path creation for downloaded doujins to have code-title whereas before it was only title. This prevents issues due to duplicate titles

--- download_manager/download_path.py
+++ download_manager/download_path.py
@@ -1,13 +1,13 @@
 import os
 import re
 
-def construct_npath(paths, base_folder="Saved_Doujins",folder_max_len=75):
+def construct_npath(paths, sauce, base_folder="Saved_Doujins",folder_max_len=75):
     script_dir = os.path.dirname(os.path.abspath(__file__))
     parent_dir = os.path.dirname(script_dir)
     target_folder = os.path.join(parent_dir, "Downloads", base_folder)
     os.makedirs(target_folder, exist_ok=True)
 
-    folder_name = paths[0][:folder_max_len].replace("/", "_").replace("\\", "_").replace(":", "_").replace("|", "_")
+    folder_name = sauce + "-" + paths[0][:folder_max_len].replace("/", "_").replace("\\", "_").replace(":", "_").replace("|", "_")
     folder_path = os.path.join(target_folder, folder_name)
     os.makedirs(folder_path, exist_ok=True)
 
